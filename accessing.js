ac1cb047627f019d89d30397a0a41031ca7bd15f
Mike Grilli <mikeg@downtownseattle.org>
2023-11-07 22:38:41 -0800

    updated assignment

--- accessing.js
+++ accessing.js
@@ -1,6 +1,8 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
 const header = document.getElementById("weather-head")
-header.innerText = "February 10 Weather Forecast, Seattle"
+header.innerText = "February 3 Weather Forecast, Seattle"
+
+
 // Change the styling of every element with class "sun" to set the color to "orange"
 
 const sun = document.querySelectorAll(".sun")
@@ -12,12 +14,6 @@ for (let i= 0; i < sun.length; i++) {
 const list = document.querySelectorAll("li")
 
 list[1].classList.replace("sun", "cloudy")
- console.log(list)
+list[1].removeAttribute("style")
 
- const removeBullets = document.getElementsByTagName("li")
- 
- console.log(removeBullets)
- for (let i = 0; i < removeBullets.length; i++) {
-    removeBullets[i].style.listStyle="none"
-    
- }
+console.log(list)
--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,27 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
+const main = document.querySelector('main')
+const aTag = document.createElement('a')
 
+aTag.setAttribute('id', 'cta')
+aTag.textContent = "Buy Now!"
+main.appendChild(aTag)
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
 
+const dataAttr = document.querySelector('img')
+console.log(dataAttr.dataset.color)
+
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
-
+const listItems = document.querySelectorAll("li")
+listItems[2].classList.add("highlight")
+console.log(listItems)
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+
+const para = document.querySelector('p')
+para.remove()
