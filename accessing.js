e5b9e4c1a939a2e908f4cef063b91921f4e82c22
Mike Grilli <mikeg@downtownseattle.org>
2023-11-11 23:43:27 -0800

    updated Assignment

--- accessing.js
+++ accessing.js
@@ -6,14 +6,14 @@ header.textContent = "February 10 Weather Forecast, Seattle"
 // Change the styling of every element with class "sun" to set the color to "orange"
 
 const sun = Array.from(document.querySelectorAll(".sun"))
-sun.forEach(els => {
-    els.style.color = 'orange'
+sun.forEach(el => {
+    el.style.color = 'orange'
 })
 
 // Change the class of the second <li> from to "sun" to "cloudy"
-const list = document.querySelectorAll("li")
+const listItem = document.querySelectorAll("li")
 
-list[1].classList.replace("sun", "cloudy")
-list[1].removeAttribute("style")
+listItem[1].classList.replace("sun", "cloudy")
+listItem[1].removeAttribute("style")
 
 // console.log(list)
--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -4,17 +4,18 @@
 //access body tag and set syles
 const body = document.querySelector('body')
 body.style.fontFamily = 'Helvetica'
-body.style.backgroundColor = "#333"
+body.style.backgroundColor = '#003b73'
 
 //create main Section for all content
 const mainSection = document.createElement('main')
-mainSection.style.color ='#333'
+mainSection.style.color ='#3d3d3d'
 mainSection.style.width = '375px'
 mainSection.style.backgroundColor = '#f2f2f2'
 mainSection.style.margin = '50px auto'
 mainSection.style.textAlign = 'center'
 mainSection.style.padding = "20px"
 mainSection.style.borderRadius = '10px'
+mainSection.style.boxShadow = '0 8px 6px -6px #3d3d3d'
 
 //append mainSection to body
 body.appendChild(mainSection)
@@ -58,40 +59,65 @@ mainSection.appendChild(buttonContainer)
 
 //create add button 
 const addButton = document.createElement('button');
-addButton.style.backgroundColor = 'green'
+addButton.style.backgroundColor = '#228b22'
 addButton.style.border = 'none'
 addButton.style.borderRadius = '5%'
 addButton.style.padding= '10px 20px'
 addButton.style.color = '#f2f2f2'
+addButton.style.boxShadow = 'inset 0 0 5px #3d3d3d'
 addButton.textContent = 'Add One'
 
 //append add button to button container
 buttonContainer.appendChild(addButton)
 
-
+//Add button hover effect
+addButton.addEventListener('mouseover', () => {
+    addButton.style.backgroundColor = '#2EFF2E'
+})
+addButton.addEventListener('mouseout', () => {
+    addButton.style.backgroundColor = '#228b22'
+})
 //create reset button 
 const resetButton= document.createElement('button');
-resetButton.style.backgroundColor = '#333'
+resetButton.style.backgroundColor = '#003b73'
 resetButton.style.border = 'none'
 resetButton.style.borderRadius = '5%'
 resetButton.style.padding= '10px 20px'
 resetButton.style.color = '#f2f2f2'
+resetButton.style.boxShadow = 'inset 0 0 5px #003b73'
 resetButton.textContent = 'Reset Counter'
 
 //append add button to button container
 buttonContainer.appendChild(resetButton)
 
+//Add button hover effect
+resetButton.addEventListener('mouseover', () => {
+    resetButton.style.backgroundColor = '#0000ff'
+})
+resetButton.addEventListener('mouseout', () => {
+    resetButton.style.backgroundColor = '#003b73'
+})
 //create subtract button 
 const subtractButton = document.createElement('button');
-subtractButton.style.backgroundColor = "red"
+
+subtractButton.style.backgroundColor = "#ae0000"
 subtractButton.style.border = 'none'
 subtractButton.style.borderRadius = '5%'
 subtractButton.style.padding= '10px 20px'
 subtractButton.style.color = '#f2f2f2'
+subtractButton.style.boxShadow = 'inset 0 0 5px #3d3d3d'
 subtractButton.textContent = 'Subtract One'
 
 //append subtract button container
 buttonContainer.appendChild(subtractButton)
+
+//Add button hover effect
+subtractButton.addEventListener('mouseover', () => {
+    subtractButton.style.backgroundColor = '#ff0000'
+})
+subtractButton.addEventListener('mouseout', () => {
+    subtractButton.style.backgroundColor = '#ae0000'
+})
 
 const footer = document.createElement('p')
 footer.style.color = '#f2f2f2'
@@ -120,19 +146,19 @@ function addOrMisusOne() {
     }
     
     if( counter < 0 ) {
-        number.style.color ='red'
-        headline.style.color = 'red'
-        body.style.backgroundColor = 'red'
+        number.style.color ='#EE4B2B'
+        headline.style.color = '#EE4B2B'
+        body.style.backgroundColor = '#EE4B2B'
     } 
     else if( counter > 0) {
-        number.style.color ='green'
-        headline.style.color = 'green'
-        body.style.backgroundColor = 'green'
+        number.style.color ='#228b22'
+        headline.style.color = '#228b22'
+        body.style.backgroundColor = '#228b22'
     }
     else {
-        number.style.color ='#333'
-        headline.style.color = '#333'
-        body.style.backgroundColor = '#333'
+        number.style.color ='#003b73'
+        headline.style.color = '#003b73'
+        body.style.backgroundColor = '#003b73'
     }
 }
 
