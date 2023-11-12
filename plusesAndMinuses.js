9d733820a0e5dbb17982e16cce4af23178b66753
Mike Grilli <mikeg@downtownseattle.org>
2023-11-12 12:33:47 -0800

    Update Assignment

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -3,26 +3,42 @@
 
 //access body tag and set syles
 const body = document.querySelector('body')
-body.style.fontFamily = 'Helvetica'
-body.style.backgroundColor = '#003b73'
+
+//Add styles
+body.style.cssText = `
+    font-family: Roboto;
+    background-color: #003b73
+`
 
 //create main Section for all content
 const mainSection = document.createElement('main')
-mainSection.style.color ='#3d3d3d'
-mainSection.style.width = '375px'
-mainSection.style.backgroundColor = '#f2f2f2'
-mainSection.style.margin = '50px auto'
-mainSection.style.textAlign = 'center'
-mainSection.style.padding = "20px"
-mainSection.style.borderRadius = '10px'
-mainSection.style.boxShadow = '0 8px 6px -6px #3d3d3d'
+
+//Add stles
+mainSection.style.cssText = `
+    color: #003b73;
+    width: 375px;
+    background-color: #f2f2f2;
+    margin: 50px auto;
+    text-align: center;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 8px 6px -6px #3d3d3d;
+`
 
 //append mainSection to body
 body.appendChild(mainSection)
 
 //create headline and add styles
 const headline = document.createElement('h1')
-headline.textContent = 'Add or Subtract by 1'
+headline.textContent = 'Counter App'
+
+//Add styles
+headline.style.cssText = `
+    font-weight: 100;
+    font-size: 4.25rem;
+    font-family: 'Lobster';
+    margin: 0 0 25px 0
+`
 
 //append headline to body
 mainSection.appendChild(headline)
@@ -37,10 +53,14 @@ mainSection.appendChild(display)
 //create p for display
 const number = document.createElement('p')
 number.textContent = 0
-number.style.textAlign = 'center'
-number.style.fontSize = '12rem'
-number.style.padding = '0'
-number.style.margin = '0 0 100px 0'
+
+//Add styles
+number.style.cssText = `
+    text-align: center;
+    font-size: 12rem;
+    padding: 0;
+    margin: -25px 0 100px 0
+`
 
 //append number to display
 display.appendChild(number)
@@ -48,65 +68,80 @@ display.appendChild(number)
 //create button container
 const buttonContainer = document.createElement('div')
 buttonContainer.classList.add('button-container')
-buttonContainer.style.height = '50px'
-buttonContainer.style.display ='flex'
-buttonContainer.style.justifyContent = 'space-evenly'
-buttonContainer.style.marginTop ='50px'
-buttonContainer.style.marginBottom = '15px'
+
+//Add styles
+buttonContainer.style.cssText = `
+    height: 50px;
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 50px;
+    margin-bottom: 15px
+`
 
 //append button container
 mainSection.appendChild(buttonContainer)
 
 //create add button 
 const addButton = document.createElement('button');
-addButton.style.backgroundColor = '#228b22'
-addButton.style.border = 'none'
-addButton.style.borderRadius = '5%'
-addButton.style.padding= '10px 20px'
-addButton.style.color = '#f2f2f2'
-addButton.style.boxShadow = 'inset 0 0 5px #3d3d3d'
 addButton.textContent = 'Add One'
+
+//add styles
+addButton.style.cssText = `
+   background-color: #228b22;
+   border: none;
+   border-radius: 5%;
+   padding: 10px 20px;
+   color: #f2f2f2;
+   box-shadow: inset 0 0 5px #3d3d3d;
+`
 
 //append add button to button container
 buttonContainer.appendChild(addButton)
 
 //Add button hover effect
 addButton.addEventListener('mouseover', () => {
-    addButton.style.backgroundColor = '#2EFF2E'
+    addButton.style.backgroundColor = '#00D100'
 })
 addButton.addEventListener('mouseout', () => {
     addButton.style.backgroundColor = '#228b22'
 })
 //create reset button 
 const resetButton= document.createElement('button');
-resetButton.style.backgroundColor = '#003b73'
-resetButton.style.border = 'none'
-resetButton.style.borderRadius = '5%'
-resetButton.style.padding= '10px 20px'
-resetButton.style.color = '#f2f2f2'
-resetButton.style.boxShadow = 'inset 0 0 5px #003b73'
 resetButton.textContent = 'Reset Counter'
+
+//add styles
+resetButton.style.cssText = `
+  background-color: #003b73;
+  border: none;
+  border-radius: 5%;
+  padding: 10px 20px;
+  color: #f2f2f2;
+  box-shadow: inset 0 0 5px #3d3d3d;
+`
 
 //append add button to button container
 buttonContainer.appendChild(resetButton)
 
 //Add button hover effect
 resetButton.addEventListener('mouseover', () => {
-    resetButton.style.backgroundColor = '#0000ff'
+    resetButton.style.backgroundColor = '#004EFF'
 })
 resetButton.addEventListener('mouseout', () => {
     resetButton.style.backgroundColor = '#003b73'
 })
 //create subtract button 
 const subtractButton = document.createElement('button');
-
-subtractButton.style.backgroundColor = "#ae0000"
-subtractButton.style.border = 'none'
-subtractButton.style.borderRadius = '5%'
-subtractButton.style.padding= '10px 20px'
-subtractButton.style.color = '#f2f2f2'
-subtractButton.style.boxShadow = 'inset 0 0 5px #3d3d3d'
 subtractButton.textContent = 'Subtract One'
+
+//add styles
+subtractButton.style.cssText = `
+  background-color: #ae0000;
+  border: none;
+  border-radius: 5%;
+  padding: 10px 20px;
+  color: #f2f2f2;
+  box-shadow: inset 0 0 5px #3d3d3d;
+`
 
 //append subtract button container
 buttonContainer.appendChild(subtractButton)
@@ -120,10 +155,14 @@ subtractButton.addEventListener('mouseout', () => {
 })
 
 const footer = document.createElement('p')
-footer.style.color = '#f2f2f2'
-footer.style.textAlign = 'center'
-footer.style.marginTop = '-25px'
 footer.textContent = 'By: Mike Grilli'
+
+//Add styles
+footer.style.cssText = `
+    color: #f2f2f2;
+    text-align: center;
+    margin-top: -25px
+`
 
 //append footer to body
 body.appendChild(footer)
@@ -146,9 +185,9 @@ function addOrMisusOne() {
     }
     
     if( counter < 0 ) {
-        number.style.color ='#EE4B2B'
-        headline.style.color = '#EE4B2B'
-        body.style.backgroundColor = '#EE4B2B'
+        number.style.color ='#ae0000'
+        headline.style.color = '#ae0000'
+        body.style.backgroundColor = '#ae0000'
     } 
     else if( counter > 0) {
         number.style.color ='#228b22'
