a57e1c6beb4a8006e7595b01b826cca102bacd7f
Mike Grilli <mikeg@downtownseattle.org>
2023-11-13 08:04:14 -0800

    Updated assignment

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -7,18 +7,9 @@ document.getElementById('cat').addEventListener('click', (e) => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
-const moreInfo = document.querySelector('#more-info')
-moreInfo.addEventListener('click', (e) => {
-  e.preventDefault()
-  alert("Here's some info")
-})
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
 
-document.getElementById('dog').addEventListener('click', (e) => { 
-  e.preventDefault()
-  alert("Bow Wow!")
-  e.stopPropagation()
-});
+
 
